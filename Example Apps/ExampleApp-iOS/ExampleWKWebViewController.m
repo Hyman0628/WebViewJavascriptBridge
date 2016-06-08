c4f2e53f08558ea3951a17d3bc864978314ff5f5
XHF <XHF@163.com>
2016-06-08 22:34:34 +0800

    添加中文注释

--- Example Apps/ExampleApp-iOS/ExampleWKWebViewController.m
+++ Example Apps/ExampleApp-iOS/ExampleWKWebViewController.m
@@ -4,7 +4,9 @@
 //
 //  Created by Marcus Westin on 1/13/14.
 //  Copyright (c) 2014 Marcus Westin. All rights reserved.
-//
+
+//官方Demo添加中文注释--XanderXu
+//https://github.com/XanderXu/WebViewJavascriptBridge.git
 
 #import "ExampleWKWebViewController.h"
 #import "WKWebViewJavascriptBridge.h"
@@ -23,17 +25,22 @@
     WKWebView* webView = [[NSClassFromString(@"WKWebView") alloc] initWithFrame:self.view.bounds];
     webView.navigationDelegate = self;
     [self.view addSubview:webView];
+     // 开启日志，方便调试
     [WKWebViewJavascriptBridge enableLogging];
+    // 给哪个webview建立JS与OjbC的沟通桥梁
     _bridge = [WKWebViewJavascriptBridge bridgeForWebView:webView];
-    
+    //注册oc方法名,,用来给js调用
     [_bridge registerHandler:@"testObjcCallback" handler:^(id data, WVJBResponseCallback responseCallback) {
-        NSLog(@"testObjcCallback called: %@", data);
-        responseCallback(@"Response from testObjcCallback");
+        NSLog(@"testObjcCallback js传过来的data: %@", data);
+        responseCallback(@"OC方法testObjcCallback的响应response");
     }];
     
+    //页面一出现就调用js的testJavascriptHandler方法,并传过去参数data
     [_bridge callHandler:@"testJavascriptHandler" data:@{ @"foo":@"before ready" }];
     
+    //添加两个OC的按钮
     [self renderButtons:webView];
+    //载入html页面
     [self loadExamplePage:webView];
 }
 
@@ -45,31 +52,38 @@
     NSLog(@"webViewDidFinishLoad");
 }
 
+//添加两个OC的按钮
 - (void)renderButtons:(WKWebView*)webView {
     UIFont* font = [UIFont fontWithName:@"HelveticaNeue" size:12.0];
-    
+    //OC按钮,点击调用js
     UIButton *callbackButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [callbackButton setTitle:@"Call handler" forState:UIControlStateNormal];
+    [callbackButton setTitle:@"OC按钮,调用js" forState:UIControlStateNormal];
     [callbackButton addTarget:self action:@selector(callHandler:) forControlEvents:UIControlEventTouchUpInside];
     [self.view insertSubview:callbackButton aboveSubview:webView];
-    callbackButton.frame = CGRectMake(10, 400, 100, 35);
+    callbackButton.frame = CGRectMake(10, 400, 140, 35);
     callbackButton.titleLabel.font = font;
     
+    //刷新按钮,点击刷新webview
     UIButton* reloadButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [reloadButton setTitle:@"Reload webview" forState:UIControlStateNormal];
+    [reloadButton setTitle:@"刷新 webview" forState:UIControlStateNormal];
+    //调用WKWebView的reload方法,重新载入页面
     [reloadButton addTarget:webView action:@selector(reload) forControlEvents:UIControlEventTouchUpInside];
     [self.view insertSubview:reloadButton aboveSubview:webView];
-    reloadButton.frame = CGRectMake(110, 400, 100, 35);
+    reloadButton.frame = CGRectMake(150, 400, 100, 35);
     reloadButton.titleLabel.font = font;
 }
 
+//点击OC按钮时,用bridge调用js方法
 - (void)callHandler:(id)sender {
     id data = @{ @"greetingFromObjC": @"Hi there, JS!" };
+    //调用js的testJavascriptHandler方法,传入data参数,并拿到js的响应response
     [_bridge callHandler:@"testJavascriptHandler" data:data responseCallback:^(id response) {
-        NSLog(@"testJavascriptHandler responded: %@", response);
+        NSLog(@"testJavascriptHandler js的响应: %@", response);
     }];
 }
 
+
+//载入网页ExampleApp.html
 - (void)loadExamplePage:(WKWebView*)webView {
     NSString* htmlPath = [[NSBundle mainBundle] pathForResource:@"ExampleApp" ofType:@"html"];
     NSString* appHtml = [NSString stringWithContentsOfFile:htmlPath encoding:NSUTF8StringEncoding error:nil];
